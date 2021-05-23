eac7354139e6d116d7bd1c24899de056bec73df3
Andre Vitor Rampanelli <andrerampanelli1@gmail.com>
2021-05-23 20:30:01 -0300

    Too tired to write descriptive message

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
+)
+
+var (
+	clientID     = "myclient"
+	clientSecret = "4678a287-edfc-4243-8d00-0caa5422baea"
+)
+
+func main() {
+	var config oauth2.Config
+	var state string
+
+	ctx := context.Background()
+
+	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/myrealm")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config = oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  "http://localhost:8081/auth/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "roles"},
+	}
+
+	state = "123"
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		http.Redirect(rw, r, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	http.HandleFunc("/auth/callback", func(rw http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("state") != state {
+			http.Error(rw, "Invalid State", http.StatusBadRequest)
+			return
+		}
+
+		token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(rw, "Invalid token", http.StatusInternalServerError)
+			return
+		}
+
+		idToken, ok := token.Extra("id_token").(string)
+		if !ok {
+			http.Error(rw, "Invalid IDToken", http.StatusInternalServerError)
+			return
+		}
+
+		userInfo, err := provider.UserInfo(ctx, oauth2.StaticTokenSource(token))
+		if err != nil {
+			http.Error(rw, "Invalid token", http.StatusInternalServerError)
+			return
+		}
+
+		resp := struct {
+			AccessToken *oauth2.Token
+			IDToken     string
+			UserInfo    *oidc.UserInfo
+		}{
+			token,
+			idToken,
+			userInfo,
+		}
+
+		data, err := json.Marshal(resp)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		rw.Write(data)
+
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
